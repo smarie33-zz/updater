3782478377932476b0815e0823eb20de3098924b
shannon <shannon@mosleygraphics.com>
2018-01-26 03:16:35 -0500

    finished updater test with readme file. '

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # updater
-Test for Updater: sass, javascript
+Test for Updater: sass, vue
+
+### Acceptance Criteria
++ All properties are required except the  promoMessage ,  promoCode ,  features  and profileHref . If any of the optional properties are empty, then the component should not render that particular section of the card.
++ The  View Profile  and  CTA  links open in a separate tab.
++ The  Features  section and  View Profile  link are hidden on the mobile view as shown in
+the mobile image above.
+
+#### Why you chose the technology you did to build the component in
++ I choose Vue because it is a component based Javascript framework that accepts objects as data that it decimenates to different parts of its templates.
+
+#### Your thought process behind the structure of the component.
++ Classes are used for common style properties that multiple elements can have; like padding and color.
++ Each object property also gets its own class so specific styling can be applied to each area.
++ `v-if` directives are used to switch between padding only divs and object derived elements depending on if the corresponding property is in the object.
++ The profile link and CTA are wrapped in a div that sits at the bottom of the card so that the bottom of every card is aligned no matter what the spacing is above.
++ ES6 is used to make all cards the same heigh on page load and browser resize.
+
+#### How to use the component
++ Use the component in your vue page by adding the `<updater-card></updater-card>` tags
++ pass an object that contains the information for one or more cards
++ use the `v-for` directive to iterate over the object and print one or more cards
+
+### Run
+`npm install`
+`$   gulp`
+OR
+`$   npm build`
--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ Vue.component('updater-card', {
 	template: `
 		<div class="card">
 			<div class="info">
-				<div class="logo"><img :src="''+info.logoUrl+''" alt=""></div>
-				<div class="company text-center text-bold text-gray">{{info.name}}</div>
-				<div class="promo text-center" v-if="info.promoMessage != ''">{{info.promoMessage}}</div>
-				<div class="promo-code text-center" v-if="info.promoCode != ''">Use promo code:<span class="bold">{{info.promoCode}}</span></div>
-				<ul class="features" v-if="info.features.length > 0">
+				<div class="logo padding-bottom-1"><img :src="''+info.logoUrl+''" alt=""></div>
+				<div class="company text-center text-bold text-gray padding-bottom-2">{{info.name}}</div>
+				<div class="promo text-center text-blue padding-bottom-1" v-if="info.hasOwnProperty('promoMessage') && info.promoMessage != ''">{{info.promoMessage}}</div>
+				<div class="promo-code text-center padding-bottom-2" v-if="info.hasOwnProperty('promoCode') && info.promoCode != ''">Use promo code: <span class="text-bold">{{info.promoCode}}</span></div>
+				<div class="padding-top-4" v-if="!info.hasOwnProperty('promoCode') || info.promoCode == ''"></div>
+				<ul class="features padding-bottom-5" v-if="info.hasOwnProperty('features') && info.features.length > 0">
 					<li v-for="feature in info.features"><i class="fa fa-check" aria-hidden="true"></i> {{feature}}</li>
 				</ul>
-				<a target="_blank" :href="''+info.profileHref+''" class="profile text-bold text-center"  v-if="info.profileHrefe != ''">View Profile</a>
-				<a target="_blank" :href="''+info.ctaUrl+''" class="button text-bold text-center">{{info.ctaText}}</a>
+				<div class="bottom">
+					<a target="_blank" :href="''+info.profileHref+''" class="profile text-bold text-center padding-bottom-2" v-if="info.hasOwnProperty('profileHref') && info.profileHref != ''">View Profile</a>
+					<div class="button-center">
+						<a target="_blank" :href="''+info.ctaUrl+''" class="button text-bold text-center">{{info.ctaText}}</a>
+					</div>
+				</div>				
 			</div>
 		</div>
 		`
@@ -38,7 +43,7 @@ new Vue({
 				profileHref :   'https://www.pensketruckrental.com/quote/start.html'
 			},
 			{
-				ctaUrl :   'https://www.updater.com',
+				ctaUrl :   'https://www.simplemovinglabor.com/booking-form/#service-info',
 				ctaText :   'Hire Labor',
 				logoUrl :   'http://res.cloudinary.com/updater-staging/image/upload/c_scale,h_70/sml.png', 
 				promoMessage :   '15% off any rental',
@@ -49,8 +54,42 @@ new Vue({
 				 'Flexible reservation and cancellation policies'
 				 ], 
 				name :   'sml',
-				profileHref :   'https://www.pensketruckrental.com/quote/start.html'
+				profileHref :   'https://www.simplemovinglabor.com/'
 			}
 		]
+	},
+	mounted: function() {
+		Vue.nextTick(() => {
+			this.changeHeight();
+	    });
+	  	window.addEventListener('resize', this.handleResize)
+	},
+	ready: function () {
+	  
+	},
+	methods: {
+		handleResize(event) {
+			this.clearHeight();
+			this.changeHeight();
+		},
+		changeHeight: function(){
+			const squares = document.querySelectorAll('.card');
+			let highest;
+			let heights = [];
+			for (let i = 0; i < squares.length; ++i) {
+				heights.push(squares[i].clientHeight);
+			}
+	      	highest = Math.max(...heights);
+	      	for (let x = 0; x < squares.length; ++x) {
+				squares[x].style.height = highest;
+			}
+		},
+		clearHeight: function(){
+			const squares = document.querySelectorAll('.card');
+			for (let x = 0; x < squares.length; ++x) {
+				squares[x].style.height = 'auto';
+			}
+			//;
+		}
 	}
 })
